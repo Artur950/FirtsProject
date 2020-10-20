54f47e672e2337ad3d33bd73a2071de011eab258
Artur <artur167950@yandex.ru>
2020-10-20 20:01:46 +0300

    Внесены иззменения в задания 2

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
+def print_hi():
+    a_tuple = (10, 20, 30, 40)
+    print(a_tuple)
 
-def print_hi(name):
-    aTuple = (10, 20, 30, 40)
-    print(aTuple)
-
-    a = aTuple[0]
-    b = aTuple[1]
-    c = aTuple[2]
-    d = aTuple[3]
+    a = a_tuple[0]
+    b = a_tuple[1]
+    c = a_tuple[2]
+    d = a_tuple[3]
 
     print(a, b, c, d)
 
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-    #
+print_hi()
