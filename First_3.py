dd32a1ff919358e2b86e8377dae655ed1dcdbc71
Artur <artur167950@yandex.ru>
2020-10-20 20:42:54 +0300

    3 задание

---
+++ First_3.py
@@ -0,0 +1,12 @@
+def delete_empty_tuple():
+    aList = [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
+    print(aList)
+    for i in aList:
+        if len(i) == 0:
+            aList.remove(i)
+        else:
+            print(i)
+    print(aList)
+
+
+delete_empty_tuple()
