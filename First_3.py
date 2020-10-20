a5ced991459489887ae426c96747ea8fc9b84c9e
Artur <artur167950@yandex.ru>
2020-10-20 22:54:26 +0300

    Внесены иззменения в задания 3

--- First_3.py
+++ First_3.py
@@ -7,6 +7,10 @@ def delete_empty_tuple():
         else:
             print(i)
     print(aList)
-
-
+    for i in aList:
+        if len(i) == 0:
+            aList.remove(i)
+        else:
+            print(i)
+    print(aList)
 delete_empty_tuple()
