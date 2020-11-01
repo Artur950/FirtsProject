189373d28a0f0d1c091185d41ffdadc88884e8e9
Artur <artur167950@yandex.ru>
2020-11-01 20:00:42 +0300

    доп.палин

--- Slade_2/Second_6.py
+++ Slade_2/Second_6.py
@@ -1,7 +1,8 @@
 def is_palindrome():
     source_string = "Чин зван мечем навзнич"
-    invert_string = ''.join(reversed(source_string))
-    if source_string == invert_string[::-1]:
+    tmp_string = source_string.replace(' ', '')
+    invert_string = ''.join(reversed(tmp_string))
+    if tmp_string.casefold() == invert_string.casefold():
         print("Является палиндромом")
     else:
         print("Не является палиндромом")
