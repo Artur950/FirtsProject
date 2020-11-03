252cdc662017151940f7968dd9c43f6b67ed21d6
Artur <artur167950@yandex.ru>
2020-11-03 20:02:48 +0300

    Закончил задание 4

--- Slade_1/First_4.py
+++ Slade_1/First_4.py
@@ -1,17 +1,22 @@
+#
 def english_russian_dictionary():
-    dictionary = {"apple": "яблоко",
-                  "age": "возраст",
-                  "answer": "ответ",
-                  "bank": "банк",
-                  "chess": "шахматы"}
-    print({v:k for k, v in dictionary.items()})
-    dictionary2 = {"apple": "яблоко",
-                  "age": "возраст",
-                  "answer": "ответ",
-                  "bank": "банк",
-                  "chess": "шахматы"}
-    dictionary2.update(dictionary)
-    print(dictionary)
+    dictionary_english_russian = {"apple": "яблоко",
+                                  "age": "возраст",
+                                  "answer": "ответ",
+                                  "bank": "банк",
+                                  "chess": "шахматы",
+                                  "humble": "скромный",
+                                  "have": "иметь",
+                                  "cosmic": "космический",
+                                  "cost": "стоить",
+                                  "fish": "рыба"}
+    print(dictionary_english_russian)
+    dictionary_russian_english = {v: k for k, v in dictionary_english_russian.items()}
+    print(dictionary_russian_english)
+    russian_words_list = [k for k in dictionary_russian_english.keys()]
+    print(sorted(russian_words_list))
+    english_words_list = [k for k in dictionary_english_russian.keys()]
+    print(sorted(english_words_list))
 
 
 english_russian_dictionary()
