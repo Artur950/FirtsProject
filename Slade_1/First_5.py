67affd1f92ad8ba70bf7514c9ea1618777fac379
Artur <artur167950@yandex.ru>
2020-11-03 20:24:16 +0300

    Вариант решения задани 5

--- Slade_1/First_5.py
+++ Slade_1/First_5.py
@@ -4,12 +4,28 @@ def count_the_characters():
     count_characters2 = ("22222222", "22222222")
     count_characters3 = ("------------------")
     count_characters4 = ("", "")
+
+    counter = 0
     for s in count_characters1:
-        print(f'{s} = {len(s)}')
+        counter = counter + len(s)
+    print('count_characters1 = ("###", "###", "###")->' + str(counter))
+
+    counter = 0
+    for s in count_characters2:
+        counter = counter + len(s)
+    print('count_characters2 = ("22222222", "22222222")->' + str(counter))
+
+    counter = 0
+    for s in count_characters3:
+        counter = counter + len(s)
+    print('count_characters3 = ("------------------")->' + str(counter))
+
+    counter = 0
+    for s in count_characters4:
+        counter = counter + len(s)
+    print('count_characters4 = ("", "")->' + str(counter))
 
 
 count_the_characters()
 
 
-#     for s in count_characters1:
-#         print(f'{s} = {len(s)}')
