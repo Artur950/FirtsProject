bb5bd762783e83f0522d9f9eed9721deacd1f57f
Artur <artur167950@yandex.ru>
2020-12-20 15:33:06 +0300

    Итог

--- Kontrolnay_rabota/CurrencyConverter.py
+++ Kontrolnay_rabota/CurrencyConverter.py
@@ -1,3 +1,4 @@
+# Описание логики работы конвертора
 # -*- coding: utf-8 -*-
 import sys
 from Parser import Parser
@@ -6,33 +7,53 @@ from PyQt5 import QtWidgets
 
 
 class CurrencyConv(QtWidgets.QMainWindow):
+    # Класса конвертера валют будет содержать одно поле,
+    # в которое будет передаваться словарь с кодами валют и их курсом
+    # из Parser
     current_converted_price = {}
 
+# Метод инициализации объекта CurrencyConv
     def __init__(self):
+        # Вызов метода инициализации объекта-родителя
         super(CurrencyConv, self).__init__()
+        # Создание объекта типа Parser
         parser = Parser()
+        # Получение словаря с кодами валют и их курсами и запись в соответствующее поле
         self.current_converted_price = parser.get_currencies()
+        # Создание и инициализация окна графического интерфейса
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.init_ui()
 
+# Метод инициализации окна графического интерфейса
     def init_ui(self):
+        # Присвоение заголовка окну приложения
         self.setWindowTitle('Конвертер валют')
-
+        # Внесение списка кодов валют в выпадающий список конвертируемой валюты
         self.ui.comboBox_input.addItems(self.current_converted_price.keys())
+        # Поле указания количесво конвертируемой валюты
         self.ui.input_currency.setPlaceholderText('Сколько конвертировать?')
 
+        # Внесение списка кодов валют в выпадающий список
         self.ui.comboBox_output.addItems(self.current_converted_price.keys())
+        # Поле в котором отображается полученное количество валюты с учетом курса
         self.ui.output_currency.setPlaceholderText('Сегодня ВЫ получите')
+        # Вызов метода конвертации при нажатии на кнопку
         self.ui.convert.clicked.connect(self.converter)
 
+# Метод в котором описано логика конвертации валют
     def converter(self):
+        # Чтение значения выбранной валюты для конвертации
         input_currency = self.ui.comboBox_input.currentText()
+        # Чтение значения количества выбранной валюты для конвертации
         input_value = self.ui.input_currency.text()
 
+        # Чтение значения выбранной валюты в которую осуществляется конвертация
         output_currency = self.ui.comboBox_output.currentText()
+        # Вычисление количества валюты после конвертации с точностью до второго знака после запятой
         output_value = round((float(input_value) * float(self.current_converted_price[input_currency])) \
                        / float(self.current_converted_price[output_currency]), 2)
+        # Вывод полученного значения конвертированной валюты в виде строки
         self.ui.output_currency.setText(str(output_value))
 
 
--- Kontrolnay_rabota/Parser.py
+++ Kontrolnay_rabota/Parser.py
@@ -1,11 +1,15 @@
+# мы осуществляем парсинг страницы на которой присутствует актуальный курс валют
 # -*- coding: utf-8 -*-
 import requests  # Модуль для обработки URL
 from bs4 import BeautifulSoup  # Модуль для работы с HTML
 
 
 class Parser:
+    # Словарь, в которой записывается ключ - 'user agent' - клиентское приложение,
+    # а значение - используемое приложение
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/87.0.4280.66 Safari/537.36'}
+    # Адрес для получения актуального курса валют
     address_bank_of_russia = 'http://www.cbr.ru/scripts/XML_daily.asp'
 
     def get_currencies(self):
@@ -13,6 +17,8 @@ class Parser:
         resp = requests.get(self.address_bank_of_russia, self.headers)
         # Разбираем через BeautifulSoup
         soup = BeautifulSoup(resp.content, 'xml')
+        # Находим все значения в полях <CharCode>...</CharCode>
+        # и <Value>...</Value>
         char_code_currencies = soup.find_all('CharCode')
         value_currencies = soup.find_all('Value')
         # Список кодов валют
@@ -30,4 +36,5 @@ class Parser:
         list_value.clear()
         list_currencies.clear()
         # В итоге функция вернет созданный нами словарь
+        # где ключи - это коды валют, а значения - актуальный курс данной валюты в рублях
         return dict_currencies
